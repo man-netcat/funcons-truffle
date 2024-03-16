0d04ceed81b50e4aa6473ea9f2ca6d0e56fe61fc
Rick Teuthof <rmt22198@outlook.com>
2024-03-17 00:23:37 +0100

    Super ultra mega testing suite

--- tests/test.py
+++ tests/test.py
@@ -1,75 +1,100 @@
+import configparser
+import glob
+import os
+from collections import defaultdict
 from unittest import TestCase, main
 
 from funcon_parser import (
+    alias_parser,
     datatype_parser,
     entity_parser,
-    alias_parser,
     funcon_def_parser,
     funcon_rule_parser,
     metavariables_parser,
     type_def_parser,
 )
-from pyparsing import ParseException
+from pyparsing import (
+    Combine,
+    Keyword,
+    OneOrMore,
+    Optional,
+    Or,
+    ParseException,
+    Suppress,
+    White,
+    ZeroOrMore,
+    cStyleComment,
+    oneOf,
+    restOfLine,
+    LineEnd,
+)
 
-test_cases = {
-    funcon_def_parser: [
-        "Funcon print(_:values*) : =>null-type",
-        "Funcon left-to-right(_:(=>(T)*)*) : =>(T)*",
-        "Funcon right-to-left(_:(=>(T)*)*) : =>(T)*",
-        "Funcon sequential(_:(=>null-type)*, _:=>T) : =>T",
-        "Funcon effect(V*:T*) : =>null-type ~> null-value",
-        "Funcon choice(_:(=>T)+) : =>T",
-        "Funcon initialise-giving(X:( )=>T') : ( )=>T' ~> no-given(X)",
-        "Funcon yield : =>null-type ~> yield-on-value(null-value)",
-        "Funcon stuck : =>empty-type",
-        "Funcon yield-on-abrupt(_:=>T) : =>T",
-        "Funcon yield-on-value(_:T) : =>T",
-        "Funcon stuck : =>empty-type",
-        "Funcon finalise-abrupting(X:=>T) : =>T|null-type ~> handle-abrupt(X, null-value)",
-        "Funcon give(_:T, _:T=>T') : =>T'",
-        "Funcon given : T=>T",
-    ],
-    funcon_rule_parser: [
-        "Rule read -- standard-in?(V:~null-type) -> V",
-        "Rule read -- standard-in?(null-value) -> fail",
-        "Rule atomic(V:T) ~> V",
-        "Rule Y ---> Y' ---- left-to-right(V*:(T)*, Y, Z*) ---> left-to-right(V*, Y', Z*)",
-        "Rule force(thunk(abstraction(X))) ~> no-given(X)",
-        "Rule X ---> X' ---- sequential(X, Y+) ---> sequential(X', Y+)",
-        "Rule sequential(null-value, Y+) ~> sequential(Y+)",
-        "Rule sequential(Y) ~> Y",
-        "Rule X --abrupt(V:T),yielded(_?)-> X' ---- yield-on-abrupt(X) --abrupt(V),yielded(signal)-> yield-on-abrupt(X')",
-        "Rule element-not-in(atoms, SA) ~> A ---- < use-atom-not-in(SA:sets(atoms)) , used-atom-set(SA') > ---> < A , used-atom-set(set-insert(A, SA')) >",
-        "Rule given-value(V) |- Y ---> Y' ---- given-value(_?) |- give(V:T, Y) ---> give(V, Y')",
-    ],
-    alias_parser: [
-        "Alias env = environment",
-    ],
-    entity_parser: [
-        "Entity < _ , used-atom-set(_:sets(atoms)) > ---> < _ , used-atom-set(_:sets(atoms)) >",
-        "Entity _ --yielded(_:yielding?)-> _",
-        "Entity _ --abrupted(_:values?)-> _",
-        "Entity given-value(_:values?) |- _ ---> _",
-    ],
-    datatype_parser: [
-        "Datatype yielding ::= signal",
-        "Datatype thunks(T) ::= thunk(_:abstractions(()=>T))",
-    ],
-    metavariables_parser: [
-        "Meta-variables T, T', T'' <: values",
-    ],
-    type_def_parser: [
-        "Type locations ~> atoms",
-    ],
+funcs = {
+    "Entity": entity_parser,
+    "Meta-variables": metavariables_parser,
+    "Type": type_def_parser,
+    "Alias": alias_parser,
+    "Datatype": datatype_parser,
+    "Funcon": funcon_def_parser,
+    "Rule": funcon_rule_parser,
+    "Assert": lambda: None,
 }
+
+KEYWORD = Or([Keyword(keyword) for keyword in funcs.keys()])
+OPTKEYWORD = Or([Keyword(keyword) for keyword in ["Auxiliary", "Built-in"]])
+
+
+index = Suppress("[") + ... + Suppress("]")
+header = OneOrMore("#") + ... + LineEnd()
+multiline_comment = Suppress("/*") + ... + Suppress("*/")
+
+indented_line = Suppress(White(min=2)) + restOfLine
+component_start = Optional(OPTKEYWORD) + KEYWORD + ~oneOf("# /* */ [ ]") + restOfLine
+component = Combine(component_start + OneOrMore(indented_line), " ")
+
+parser = ZeroOrMore(component)
+parser = parser.ignore(multiline_comment | index | header)
+
+# TODO Fix comments ending up in parsed result anyway
+print(parser)
+
+
+# Function to parse file and extract components
+def parse_components(file_path):
+    with open(file_path, "r") as file:
+        try:
+            return parser.parseFile(file)
+        except ParseException as e:
+            errstr = f"\n{'-'*70}\n{ParseException.explain(e)}"
+            raise ParseException(errstr) from None
 
 
 class TestParserFunction(TestCase):
     pass
 
 
+allcases = defaultdict(lambda: [])
+config = configparser.ConfigParser()
+config.read("./tests/test.cfg")
+funcon_dir = config.get("Paths", "funcon-dir")
+pattern = os.path.join(funcon_dir, "**/*.cbs")
+cbs_files = glob.glob(pattern, recursive=True)
+
+# Parse files and extract components
+for path in cbs_files:
+    res = parse_components(path)
+    for line in res:
+        splitline = line.split()
+        keyword = splitline[0]
+        if keyword in ["Auxiliary", "Built-in"]:
+            keyword = splitline[1]
+        allcases[keyword].append(line)
+
 # Dynamically generate test functions for each test case
-for func, cases in test_cases.items():
+for keyword, cases in allcases.items():
+    for case in cases:
+        print(case)
+    func = funcs[keyword]
 
     for i, case in enumerate(cases, 1):
 
@@ -85,7 +110,6 @@ for func, cases in test_cases.items():
 
         test_func = generate_test_function(func, case)
         setattr(TestParserFunction, f"test_{func.__name__}_{i}", test_func)
-
 
 if __name__ == "__main__":
     main()
