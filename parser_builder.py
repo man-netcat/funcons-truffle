11aa1bc29303524f39a2114e39f9c0aab533558a
Rick Teuthof <rmt22198@outlook.com>
2024-04-02 15:55:49 +0200

    4 Errors remaining

--- parser_builder.py
+++ parser_builder.py
@@ -2,6 +2,7 @@ import json
 import re
 from collections import defaultdict
 from pprint import pprint
+from string import ascii_lowercase
 
 from pyparsing import (
     Combine,
@@ -22,10 +23,13 @@ from pyparsing import (
     alphanums,
     delimitedList,
     infixNotation,
+    nums,
     oneOf,
     opAssoc,
     restOfLine,
 )
+
+ParserElement.enablePackrat(None)
 
 
 def encapsulate(expr, left, right):
@@ -34,6 +38,7 @@ def encapsulate(expr, left, right):
     )
 
 
+BAR = Suppress(Literal("---") + OneOrMore("-")).setName("---")
 ASSERT = Keyword("Assert")
 TYPE = (Keyword("Type") | Keyword("Built-in Type")).setName("Type")
 DATATYPE = (Keyword("Built-in Datatype") | Keyword("Datatype")).setName("Datatype")
@@ -46,21 +51,23 @@ FUNCON = (
 ).setName("Funcon")
 
 
-KEYWORD = (ASSERT | FUNCON | TYPE | DATATYPE | ENTITY | METAVARIABLES).setName(
-    "keyword"
+BASEKEYWORD = (ASSERT | FUNCON | TYPE | DATATYPE | ENTITY | METAVARIABLES).setName(
+    "basekeyword"
 )
+EXTKEYWORD = (ALIAS | RULE).setName("extkeyword")
+KEYWORD = (BASEKEYWORD | EXTKEYWORD).setName("keyword")
 
-
-IDENTIFIER = Combine(
+IDENTIFIER = ~(KEYWORD | BAR) + Combine(
     Word(
         initChars=alphanums + "_",
-        bodyChars=alphanums + "-",
+        bodyChars=alphanums + '-"',
     )
     + ZeroOrMore("'")
 ).setName("id")
+NUMBER = Combine(Optional("-") + Word(nums))
 
 POLARITY = oneOf("! ?").setName("polarity")
-SUFFIX = oneOf("* + ?").setName("suffix")
+SUFFIX = oneOf("* + ? ^N").setName("suffix")
 PREFIX = oneOf("~ =>").setName("prefix")
 INFIX = oneOf("=> | &").setName("infix")
 
@@ -73,10 +80,7 @@ EQUALS = Suppress("==").setName("==")
 NOTEQUALS = Suppress("=/=").setName("=/=")
 EQUALITY = (EQUALS | NOTEQUALS).setName("equality")
 COLON = Suppress(":").setName(":")
-BAR = Suppress(Literal("---") + OneOrMore("-")).setName("---")
-EMPTYMAP = Group(Suppress("{") + Suppress("}"))("emptymap").setName("emptymap")
-BOOL = (Keyword("true") | Keyword("false")).setName("boolean")
-STRING = Combine(Suppress('"') + IDENTIFIER + Literal('"')).setName("string")
+COMMA = Suppress(",").setName(",")
 
 # Define forward expression for recursion
 expr = Forward().setName("expr")
@@ -86,8 +90,6 @@ param = Group(
 )
 
 params = encapsulate(delimitedList(param("params*")), "(", ")")
-
-nestedexpr = encapsulate(expr, "(", ")")
 
 fun_call = Group(IDENTIFIER("fun") + params).setName("fun_call")
 
@@ -103,58 +105,57 @@ entitysig = Group(IDENTIFIER("name") + Optional(POLARITY("polarity")) + params).
     "entity"
 )
 
-step = encapsulate(delimitedList(entitysig("entities*")), "--", "->").setName("step")
-
-mutableentity = Group(encapsulate(delimitedList(expr("mutables*")), "<", ">"))(
-    "mutableentities*"
-)
-
-# chain = Forward()
-# chain <<= Group(
-#     IDENTIFIER("fun") + (chain | fun_call | IDENTIFIER)("param"),
-# )("unary")
-
-operands = Forward()
+computation = encapsulate(delimitedList(entitysig("ioc*")), "--", "->").setName("step")
 
 operands = Or(
     [
         listindex,
         listexpr,
         mapping,
-        mutableentity,
         fun_call,
-        # chain,
-        nestedexpr,
         params,
-        STRING,
+        NUMBER,
         IDENTIFIER,
     ]
 )
 
-ParserElement.enablePackrat(None)
 expr <<= infixNotation(
     operands,
     [
         (SUFFIX, 1, opAssoc.LEFT),
         (PREFIX, 1, opAssoc.RIGHT),
+        (IDENTIFIER, 1, opAssoc.RIGHT),
         (INFIX, 2, opAssoc.LEFT),
     ],
 )
 
-contextualentity = Group(
-    Optional(expr("context") + CONTEXTUALENTITY)
-    + (expr("before") + step + expr("after"))
+# Rules
+
+mutablentitysig = Group(
+    encapsulate(expr("source") + COMMA + expr("target"), "<", ">"),
+)("mutableentity")
+mutableentity = mutablentitysig("before") + computation + mutablentitysig("after")
+
+
+# Input/Output/Control entities (with optional context)
+context = expr("context") + CONTEXTUALENTITY
+ioc_entity = Group(Optional(context) + expr("before") + computation + expr("after"))(
+    "rewrite"
 )
 
-
+# Type premise
 typeexpr = Group(expr("value") + COLON + expr("type")).setName("typeexpr")
-boolexpr = Group(expr("value") + EQUALITY + (expr | BOOL | STRING | EMPTYMAP)).setName(
-    "boolexpr"
-)
+
+# Boolean premise
+boolexpr = Group(
+    expr("value") + ((EQUALS + expr("equals")) | (NOTEQUALS + expr("notequals")))
+).setName("boolexpr")
+
+#
 rewrite = Group(expr("value") + REWRITES_TO + expr("rewrites_to")).setName(
     "rewriteexpr"
 )
-premise = Group(rewrite | boolexpr | typeexpr | contextualentity)
+premise = ioc_entity | mutableentity | rewrite | boolexpr | typeexpr
 premises = OneOrMore(premise)
 
 transition = premises("premises*") + BAR + premise("rewritten")
@@ -163,42 +164,58 @@ rule_def = Group(transition)("transition") | premise("premise")
 
 rule = Suppress(RULE) + Group(rule_def)("rules*")
 
+# Aliases
+
 alias = Suppress(ALIAS) + Group(
     IDENTIFIER("alias") + Suppress("=") + IDENTIFIER("original")
 )("aliases*")
 aliases = ZeroOrMore(alias)
 
+# Funcons
+
 funcon_def = Group(
     IDENTIFIER("name")
-    + Optional(
-        params,
-    )
+    + Optional(params)
     + COLON
     + expr("returns")
     + Optional(
         REWRITES_TO + expr("rewrites_to"),
     )
 )("definition")
-funcon_parser = Suppress(FUNCON) + funcon_def + ZeroOrMore(alias | rule)
+funcon_parser = Suppress(FUNCON) + funcon_def + ZeroOrMore(alias) + ZeroOrMore(rule)
 
-entitydef = contextualentity("rewrite") | mutableentity
+# Entities
+
+entitydef = mutableentity | ioc_entity
 entity_parser = Suppress(ENTITY) + entitydef + aliases
+
+# Types
 
 type_def = (
     expr("type")
     + Optional(METAVARASSIGN + expr("assigns"))
-    + Optional(REWRITES_TO + expr("value"))
+    + Optional(REWRITES_TO + expr("rewrites_to"))
 )
 type_parser = Suppress(TYPE) + type_def + aliases
+
+# Datatypes
 
 datatypedef = (
     expr("name") + Suppress(DATATYPEASSIGN | METAVARASSIGN) + expr("definition")
 )
 datatype_parser = Suppress(DATATYPE) + datatypedef + aliases
 
-metavar_type = expr("types*")
+
+# Metavars
+
+metavar_type = infixNotation(
+    fun_call | IDENTIFIER,
+    [
+        (SUFFIX, 1, opAssoc.LEFT),
+    ],
+)
 metavar_types = delimitedList(metavar_type)
-metavar_def = metavar_types + METAVARASSIGN + expr("definition")
+metavar_def = metavar_types("names*") + METAVARASSIGN + metavar_types("definition")
 metavar_defs = OneOrMore(metavar_def)
 metavar_parser = Suppress(METAVARIABLES) + metavar_defs
 
@@ -216,12 +233,12 @@ parsers = {
 }
 
 componentparser = ZeroOrMore(
-    Combine(KEYWORD + ... + Suppress(FollowedBy(KEYWORD | StringEnd())))
+    Combine(BASEKEYWORD + ... + Suppress(FollowedBy(BASEKEYWORD | StringEnd())))
 )
 
 
 def clean_text(string):
-    indexline = KEYWORD + IDENTIFIER + Optional(ALIAS + IDENTIFIER)
+    indexline = BASEKEYWORD + IDENTIFIER + Optional(ALIAS + IDENTIFIER)
     indexlines = OneOrMore(indexline)
 
     index = Suppress("[" + indexlines + "]")
@@ -290,10 +307,23 @@ def parse_file(path, dump_json=False, print_res=False):
         for component in components:
             try:
                 res = parse_str(parsers[keyword], component)
+                data[keyword].append(res.asDict())
             except:
                 pass
-            data[keyword].append(res.asDict())
     if print_res:
         pprint(data)
     if dump_json:
         json.dump(data)
+
+
+parse_str(
+    funcon_parser,
+    """
+Funcon
+  read : =>values
+Rule
+  read -- standard-in?(V:~null-type) -> V
+Rule
+  read -- standard-in?(null-value) -> fail
+""",
+)
