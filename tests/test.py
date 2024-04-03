f18e4ce3ac06401ff5fa39dd960dbcb9506974b4
Rick Teuthof <rmt22198@outlook.com>
2024-04-03 14:06:44 +0200

    Corrected test script

--- tests/test.py
+++ tests/test.py
@@ -18,28 +18,27 @@ pattern = os.path.join(funcon_dir, "**/*.cbs")
 cbs_files = glob.glob(pattern, recursive=True)
 
 
-def generate_test_function(parser, component):
+def generate_test_function(parser, case):
     def test_function(self):
-        parse_str(parser, component)
+        parse_str(parser, case)
 
     return test_function
 
 
-keyword_counters = defaultdict(lambda: 1)
-
+allcases = defaultdict(lambda: [])
 for file in cbs_files:
-    file_components = parse_file_components(file)
-    for keyword, component in file_components.items():
-        parser = parsers[keyword]
+    cases = parse_file_components(file)
+    for keyword, case in cases.items():
+        allcases[keyword].extend(case)
 
-        for comp in component:
-            test_func = generate_test_function(parser, comp)
-            setattr(
-                TestParserFunction,
-                f"test_{keyword.lower()}_{keyword_counters[keyword]}",
-                test_func,
-            )
 
-        keyword_counters[keyword] += 1
+# Dynamically generate test functions for each test case
+for keyword, cases in allcases.items():
+    parser = parsers[keyword]
+
+    for i, case in enumerate(cases, 1):
+        test_func = generate_test_function(parser, case)
+        setattr(TestParserFunction, f"test_{keyword.lower()}_{i}", test_func)
+
 if __name__ == "__main__":
     main()
